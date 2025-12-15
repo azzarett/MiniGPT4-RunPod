8c2ae7c06b7d4992de48b5d068921c65874f5d13
Азат <zachet04@gmail.com>
2025-12-15 16:49:51 +0500

    Optimize for inference: remove training dependencies (visual-genome, decord, wandb)

--- minigpt4/__init__.py
+++ minigpt4/__init__.py
@@ -12,10 +12,10 @@ from omegaconf import OmegaConf
 
 from minigpt4.common.registry import registry
 
-from minigpt4.datasets.builders import *
+# from minigpt4.datasets.builders import *
 from minigpt4.models import *
 from minigpt4.processors import *
-from minigpt4.tasks import *
+# from minigpt4.tasks import *
 
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
